d1f5f74b1abf2b24dfa744a7fd0263cab7a28f99
VortexZAA <abidinayhan94@gmail.com>
2023-10-27 14:15:32 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 export default function Home() {
   const [random, setRandom] = useState({
     top: 0,
     right: 0,
   });
-  const isconneted = true;
+  const [showText, setShowText] = useState(true);
+  const text =
+    "Enter, traveler, into my realm. Should you desire to wield powers most extraordinary, I have 48 different power scrolls. If you want to have one, a mere gesture towards my essence is all that is required. Click, and behold the wonders that await. P.S. Use them with wisdom, for such artifacts demand great responsibility.";
+  const typeRef: any = useRef(null);
+
+  const isconneted = false;
   /* useEffect(() => {
     let time = 700;
 
@@ -38,6 +43,11 @@ export default function Home() {
 
   useEffect(() => {
     const timer = setTimeout(incrementRandom, 1000);
+    //@ts-ignore
+    console.log(typeRef?.current?.innerHTML.length === text.length);
+
+    typeRef?.current?.innerHTML.length === text.length &&
+      setTimeout(()=>setShowText(false), 2000);
     return () => clearTimeout(timer);
   }, [random]);
 
@@ -45,13 +55,26 @@ export default function Home() {
     <main
       className={`flex min-h-screen max-w-[100vw] h-screen w-screen bg-wizardBg flex-col items-center justify-between p-3 md:p-6 relative overflow-hidden`}
     >
-      <h1 className="text-black">
-        Enter, traveler, into my realm. Should you desire to wield powers most
-        extraordinary, I have 48 different power scrolls. If you want to have
-        one, a mere gesture towards my essence is all that is required. Click,
-        and behold the wonders that await. P.S. Use them with wisdom, for such
-        artifacts demand great responsibility.
-      </h1>
+      {showText ? (
+        <h1 className="text-black font-medium opacity-80 text-2xl ">
+          <TypeAnimation
+            sequence={[
+              `${"Enter, traveler, into my realm. Should you desire to wield powers most extraordinary, I have 48 different power scrolls. If you want to have one, a mere gesture towards my essence is all that is required. Click, and behold the wonders that await. P.S. Use them with wisdom, for such artifacts demand great responsibility."}`,
+            ]}
+            wrapper="div"
+            speed={60}
+            cursor={true}
+            repeat={0}
+            ref={typeRef}
+            className="h-auto min-h-[110px] cursor"
+            omitDeletionAnimation={true}
+          />
+        </h1>
+      ) : (
+        <button className=" rounded-md p-6 bg-[url('/button.svg')] bg-no-repeat bg-contain ">
+          CONNECT WALLET
+        </button>
+      )}
 
       <button
         className="absolute w-28 bg-[url('/wizard.gif')] h-28 bg-contain bg-no-repeat z-10 "
